e5c386a7ac1bb4bd4dd95694c858b9f25722446e
Ultraflex <manzh99@gmail.com>
2022-07-12 23:12:09 +0300

    gif color

--- anim_text.py
+++ anim_text.py
@@ -4,6 +4,24 @@ frames = []
 
 
 def create_image_with_text(size, text, fnt):
+    color = [
+        ['#ccd5ae', '#fefae0'],
+        ['#ffafcc', '#a2d2ff'],
+        ['#bc6c25', '#bc6c25'],
+        ['#264653', '#e9c46a'],
+        ['#023047', '#219ebc'],
+        ['#8338ec', '#3a86ff'],
+        ['#fcbf49', '#f77f00'],
+        ['#f1faee', '#a8dadc'],
+        ['#f6bd60', 'f7ede2'],
+        ['#ef233c', '#8d99ae'],
+        ['#ffb4a2', '#ffcdb2'],
+        ['#9b5de5', '#00bbf9'],
+        ['#023e8a', '#0077b6'],
+        ['#a3b18a', '#588157'],
+        ['#d6ccc2', '#f5ebe0'],
+        ['#bb3e03', '#ca6702']
+    ]
     img = Image.new('RGB', (600, 50), '#ffbe0b')
     draw = ImageDraw.Draw(img)
     draw.fontmode = "1"
