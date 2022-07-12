8f18049c576cdd3d7a82cde09d10c27ea262e7f3
Ultraflex <manzh99@gmail.com>
2022-07-12 22:05:57 +0300

    gif1

---
+++ anim_text.py
@@ -0,0 +1,28 @@
+from PIL import Image, ImageDraw, ImageSequence
+import io
+
+im = Image.open('test.gif')
+
+# A list of the frames to be outputted
+frames = []
+# Loop over each frame in the animated image
+for frame in ImageSequence.Iterator(im):
+    # Draw the text on the frame
+    d = ImageDraw.Draw(frame)
+    d.text((10,100), "Hello World")
+    del d
+
+    # However, 'frame' is still the animated image with many frames
+    # It has simply been seeked to a later frame
+    # For our list of frames, we only want the current frame
+
+    # Saving the image without 'save_all' will turn it into a single frame image, and we can then re-open it
+    # To be efficient, we will save it to a stream, rather than to file
+    b = io.BytesIO()
+    frame.save(b, format="GIF")
+    frame = Image.open(b)
+
+    # Then append the single frame image to a list of frames
+    frames.append(frame)
+# Save the frames as a new image
+frames[0].save('out.gif', save_all=True, append_images=frames[1:])
