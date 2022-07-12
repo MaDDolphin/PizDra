caa315fd8a63e3a70d68b4234c8fa325079aa56b
Ultraflex <manzh99@gmail.com>
2022-07-12 22:32:25 +0300

    gif

--- anim_text.py
+++ anim_text.py
@@ -1,24 +1,36 @@
 from PIL import Image, ImageDraw, ImageFont
 
+
 def create_image_with_text(size, text):
-    img = Image.new('RGB', (600, 50), "yellow")
+    img = Image.new('RGB', (600, 50), '#3E8989')
     draw = ImageDraw.Draw(img)
-    draw.text((size[0], size[1]), text, font = fnt, fill="black")
+    draw.fontmode = "1"
+    draw.text((size[0], size[1]), text, font=fnt, fill="#2CDA9D")
     return img
+
 
 frames = []
 
+
 def roll(text):
-    for i in range(len(text)+1):
-        new_frame = create_image_with_text((0,0), text[:i])
+    for i in range(len(text) + 1):
+        new_frame = create_image_with_text((0, 0), text[:i])
         frames.append(new_frame)
+
+text = """ С добрым утром, улыбайся,
+Пусть удачным будет день!
+Каждым мигом наслаждайся,
+Пусть уйдет печали тень!
+
+Солнце пусть тебя согреет,
+Кофеек пускай взбодрит,
+Сердце пусть от счастья млеет,
+А в душе лишь мир царит!
+"""
 # <<< ========== Customize font and text below ============== >>>>
-fnt = ImageFont.truetype("arial", 36)
-all_text = """ Pythonprogramming
-Brought you this code
-This text was made
-with PIL and Python""".splitlines()
+fnt = ImageFont.truetype("impact", 36)
+all_text = text.splitlines()
 [roll(text) for text in all_text]
 # <<< ======================================================== >>>
-frames[0].save('banner1.gif', format='GIF',
+frames[0].save('banner.gif', format='GIF',
                append_images=frames[1:], save_all=True, duration=80, loop=0)
