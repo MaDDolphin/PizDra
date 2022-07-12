25bcca3cc4d00d51409e181f374f5bff197b3b7d
Ultraflex <manzh99@gmail.com>
2022-07-13 00:33:53 +0300

    gif layer with pic

--- anim_text.py
+++ anim_text.py
@@ -1,10 +1,13 @@
+import random
+
 from PIL import Image, ImageDraw, ImageFont
 
 frames = []
+c = 0
+inc = 10
 
-
-def create_image_with_text(size, text, fnt):
-    color = [
+def get_color():
+    colors = [
         ['#ccd5ae', '#fefae0'],
         ['#ffafcc', '#a2d2ff'],
         ['#bc6c25', '#bc6c25'],
@@ -13,7 +16,7 @@ def create_image_with_text(size, text, fnt):
         ['#8338ec', '#3a86ff'],
         ['#fcbf49', '#f77f00'],
         ['#f1faee', '#a8dadc'],
-        ['#f6bd60', 'f7ede2'],
+        ['#f6bd60', '#f7ede2'],
         ['#ef233c', '#8d99ae'],
         ['#ffb4a2', '#ffcdb2'],
         ['#9b5de5', '#00bbf9'],
@@ -22,20 +25,36 @@ def create_image_with_text(size, text, fnt):
         ['#d6ccc2', '#f5ebe0'],
         ['#bb3e03', '#ca6702']
     ]
-    img = Image.new('RGB', (600, 50), '#ffbe0b')
+    cn = random.randint(0, len(colors) - 1)
+    return colors[cn]
+
+
+'''
+def create_image_with_text(size, text, fnt, color):
+    global c, inc
+    img = Image.open("123.png")
+    w,h = img.size
+    draw = ImageDraw.Draw(img)
+    draw.text((w // 5, h // 2), text, font = fnt, fill=color[1])
+    c += inc
+    return img
+'''
+def create_image_with_text(size, text, fnt, color):
+    img = Image.new('RGB', (600, 50), color[0])
     draw = ImageDraw.Draw(img)
     draw.fontmode = "1"
-    draw.text((size[0], size[1]), text, font=fnt, fill="#fb5607")
+    draw.text((size[0], size[1]), text, font=fnt, fill=color[1])
     return img
 
 
-def roll(text, fnt):
+def roll(text, fnt, color):
     for i in range(len(text) + 1):
-        new_frame = create_image_with_text((0, 0), text[:i], fnt)
+        new_frame = create_image_with_text((0, 0), text[:i], fnt, color)
         frames.append(new_frame)
 
 
 def main():
+    color = get_color()
     text = """ С добрым утром, улыбайся,
     Пусть удачным будет день!
     Каждым мигом наслаждайся,
@@ -49,7 +68,7 @@ def main():
     # <<< ========== Customize font and text below ============== >>>>
     fnt = ImageFont.truetype("impact", 36)
     all_text = text.splitlines()
-    [roll(text, fnt) for text in all_text]
+    [roll(text, fnt, color) for text in all_text]
     # <<< ======================================================== >>>
     frames[0].save('banner.gif', format='GIF',
                    append_images=frames[1:], save_all=True, duration=80, loop=0)
--- get_frames.gif.py
+++ get_frames.gif.py
@@ -1,8 +1,44 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
-num_key_frames = 500
+c = 0
+inc = 10
 
-with Image.open('banner.gif') as im:
-    for i in range(num_key_frames):
-        im.seek(im.n_frames // num_key_frames * i)
-        im.save('{}.png'.format(i))
+
+def create_image_with_text(size, text):
+    global c, inc
+    img = Image.open("1.jpg")
+    w, h = img.size
+    draw = ImageDraw.Draw(img)
+    draw.text((0, 0), text, font=fnt, fill=('#ffffff')
+    c += inc
+    return img
+
+
+# Create the frames
+frames = []
+
+
+def roll(text):
+    global c
+    for i in range(len(text) + 1):
+        new_frame = create_image_with_text((0, 0), text[:i])
+        frames.append(new_frame)
+    c = 0
+
+
+fnt = ImageFont.truetype("impact", 35)
+all_text = """ С добрым утром, улыбайся,
+Пусть удачным будет день!
+Каждым мигом наслаждайся,
+Пусть уйдет печали тень!
+Солнце пусть тебя согреет,
+Кофеек пускай взбодрит,
+Сердце пусть от счастья млеет,
+А в душе лишь мир царит!
+    """.splitlines()
+[roll(text) for text in all_text]
+
+# Save into a GIF file that loops forever
+frames[0].save('banner_c.gif', format='GIF',
+               append_images=frames[1:], save_all=True, duration=80, loop=0)
+print("Done")
