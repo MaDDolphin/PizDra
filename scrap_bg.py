2d0ce919fe48a2bd5289248caa8028cdc0cdad88
ultraflex <manzh99@gmail.com>
2022-08-01 14:18:07 +0300

    as
    scrape gen

--- scrap_bg.py
+++ scrap_bg.py
@@ -1,9 +1,10 @@
 from typing import Dict, Any
 import json
+import shutil
 import requests
 from bs4 import BeautifulSoup
 import os
-import re
+import datetime
 
 
 def read_file(filename):
@@ -13,71 +14,54 @@ def read_file(filename):
 
 
 def html_req():
-    for page_nmb in range(1, 9):
+    for page_nmb in range(1, 18):
         print(page_nmb)
-        url = 'https://wallpaperscraft.ru/tag/завтрак/1080x1920/page%d#' % page_nmb
+        url = 'https://wallpaperscraft.ru/tag/восход/1080x1920/page%d#' % page_nmb
         r = requests.get(url)
         with open('./pages/' + str(page_nmb) + '.html', 'w', encoding="utf-8") as output_file:
             output_file.write(r.text)
 
 
-def photo_link_parse(filename, links):
+def photo_parse(filename, links):
     text = read_file(filename)
     soup = BeautifulSoup(text)
     photo_list = soup.find('div', {'class': 'wallpapers wallpapers_zoom wallpapers_main'})
-    photos = photo_list.find_all('div', {'class': 'joke mdl-shadow--6dp block mdl-card mdl-card--border'})
-    print(photo_list)
+    photos = photo_list.find_all('li', {'class': 'wallpapers__item'})
+    i = 0
     for photo in photos:
-        link = photo.find('img', {'class': 'wallpapers__image'}).get('src')
+        while os.path.exists('./bg/frame' + str(i) + '.jpg'):
+            i = i+1
+        link = photo.find('img', {'class': 'wallpapers__image'}).get('src').replace('168x300', '1080x1920')
         print(link)
+        img_data = requests.get(link).content
+        with open('./bg/frame'+str(i)+'.jpg', 'wb') as handler:
+            handler.write(img_data)
+
         links.append(link)
 
 
-def joke_parse(links):
-    print('Start joke download!')
-
-    for link in links:
-        print(id, ' joke ')
-        url = 'https://baneks.site' + link
-        r = requests.get(url)
-        with open('./jokes_pages/' + str(id) + '.html', 'w', encoding="utf-8") as output_file:
-            output_file.write(r.text)
-        id = id + 1
-    print('Jokes were loaded')
 
 
-def joke_text_parse(filename, joke_dic, id):
-    text = read_file(filename)
-    soup = BeautifulSoup(text)
-    jokes_list = soup.find('div', {'class': 'joke mdl-shadow--6dp block mdl-card mdl-card--border'})
-    jokes = jokes_list.find_all('div', {
-        'class': 'block-content mdl-card__supporting-text mdl-color--grey-300 mdl-color-text--grey-900'})
-    for joke in jokes:
-        joke_str = str(joke.find('section', {'itemprop': 'description'}).find('p'))
-        joke_str = joke_str.replace('</p>', '')
-        joke_str = joke_str.replace('<p>', '')
-        joke_str = joke_str.replace('<br/>', '\n')
-    joke_dic[id] = joke_str
-    print('Joke number ', id)
 
 
 def main():
-    html_req()
 
+    folder = '/pages'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    html_req()
     links = []
-    id = 0
-    joke_dic = {}
+
     for root, dirs, files in os.walk('./pages'):
         for file in files:
-            photo_link_parse('./pages/' + file, links)
-    joke_parse(links)
-    for root, dirs, files in os.walk('./jokes_pages'):
-        for file in files:
-            joke_text_parse('./jokes_pages/' + file, joke_dic, id)
-            id = id + 1
-    with open('jokes.json', 'w') as fp:
-        json.dump(joke_dic, fp)
-
+            photo_parse('./pages/' + file, links)
 
 if __name__ == "__main__":
     main()
