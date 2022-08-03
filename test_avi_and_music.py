246b7442babf7771aac00e07141965b6706a8bf5
ultraflex <manzh99@gmail.com>
2022-08-03 15:52:16 +0300

    Update test_avi_and_music.py

--- test_avi_and_music.py
+++ test_avi_and_music.py
@@ -1,15 +1,18 @@
 import os
-import subprocess
 import random
 import time
 import shutil
+
 date = round(time.time() * 1000)
 tmp_path = './temp/' + str(date)
+
+
 def bg_photo_name_gen(tmp_path):
     bg_names = []
     os.mkdir(tmp_path)
-    for i in range(0, 20): #6280+1
-        shutil.copyfile('./scrap_data/bg/'+str(bg_names.append(random.randint(0, 6280))), tmp_path+'/'+str())
+    for i in range(0, 20):  # 6280+1
+        shutil.copyfile('./scrap_data/bg/' + str(random.randint(0, 6280)),
+                        tmp_path + '/frame' + str(i) + '.jpg')
 
 
 bg_photo_name_gen(tmp_path)
